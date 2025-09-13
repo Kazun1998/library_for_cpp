7a1571d1d24205e92e9d660066b4aa141ac9b885
Kazun1998k <hkazu1998@gmail.com>
2025-09-13 23:31:43 +0900

    refactor: Modulo_Polynomial

---
+++ Modulo_Polynomial/Modulo_Polynomial.hpp
@@ -0,0 +1,143 @@
+#pragma once
+
+#include"../template/template.hpp"
+#include"../modint.hpp"
+
+template<const ll Mod>
+class Modulo_Polynomial {
+    public:
+    using mint = modint<Mod>;
+
+    private:
+    vector<mint> poly;
+    int precision = 0;
+
+    public:
+    Modulo_Polynomial(vector<mint> poly, int precision): poly(poly), precision(precision) {}
+
+    Modulo_Polynomial() = default;
+    Modulo_Polynomial(vector<mint> poly) : Modulo_Polynomial(poly, poly.size()) {}
+    Modulo_Polynomial(int precision) : Modulo_Polynomial({}, precision) {}
+
+    // 演算子の定義
+    public:
+    // マイナス元
+    Modulo_Polynomial operator-() const {
+        Modulo_Polynomial res(*this);
+        for (auto &a : res.poly) { a = -a; }
+        return res;
+    }
+
+    // 加法
+    Modulo_Polynomial& operator+=(const Modulo_Polynomial &P){
+        if (size() < P.size()) { resize(P.size()); }
+
+        for (int i = 0; i < (int) P.poly.size(); i++) { poly[i] += P[i]; }
+        reduce();
+
+        return *this;
+    }
+
+    Modulo_Polynomial& operator+=(const mint &a){
+        if (poly.empty()) { resize(1); }
+        poly[0] += a;
+        reduce();
+
+        return *this;
+    }
+
+    friend Modulo_Polynomial operator+(const Modulo_Polynomial &lhs, const Modulo_Polynomial &rhs) { return Modulo_Polynomial(lhs) += rhs; }
+    Modulo_Polynomial operator+(const mint &a) const { return Modulo_Polynomial(*this) += a; }
+
+    // 減法
+    Modulo_Polynomial& operator-=(const Modulo_Polynomial &P){
+        if (size() < P.size()) { resize(P.size()); }
+
+        for (int i = 0; i < (int) P.poly.size(); i++) { poly[i] -= P[i]; }
+        reduce();
+
+        return *this;
+    }
+
+    Modulo_Polynomial& operator-=(const mint &a){
+        if (poly.empty()) { resize(1); }
+        poly[0] -= a;
+        reduce();
+
+        return *this;
+    }
+
+    friend Modulo_Polynomial operator-(const Modulo_Polynomial &lhs, const Modulo_Polynomial &rhs) { return Modulo_Polynomial(lhs) -= rhs; }
+    Modulo_Polynomial operator-(const mint &a) const { return Modulo_Polynomial(*this) -= a; }
+
+    // スカラー倍
+    Modulo_Polynomial& operator*=(const mint &a){
+        for (int i = 0; i < size(); i++) { poly[i] *= a; }
+        reduce();
+        return *this;
+    }
+
+    Modulo_Polynomial operator*(const mint &a) const {return Modulo_Polynomial(*this) *= a;}
+
+    friend Modulo_Polynomial operator*(const mint &a, const Modulo_Polynomial &P) {
+        Modulo_Polynomial res(P);
+        res *= a;
+        return res;
+    }
+
+    // 積
+    Modulo_Polynomial& operator*=(const Modulo_Polynomial &P) {
+        int r = min({(int) (poly.size() + P.poly.size()) - 1, precision, P.precision});
+        vector<mint> A(r);
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < P.size(); j++) {
+                if (i + j < r) { A[i + j] += poly[i] * P.poly[j]; }
+            }
+        }
+
+        poly = A;
+        precision = min(precision, P.precision);
+        return *this;
+    }
+
+    friend Modulo_Polynomial operator*(const Modulo_Polynomial &lhs, const Modulo_Polynomial &rhs) { return Modulo_Polynomial(lhs) *= rhs; }
+
+    // スカラー除算
+    Modulo_Polynomial& operator/=(const mint &a) {
+        mint a_inv = a.inverse();
+        for (int i = 0; i < size(); i++) { poly[i] *= a_inv; }
+        return *this;
+    }
+
+    Modulo_Polynomial operator/(const mint &a) const { return Modulo_Polynomial(*this) /= a; }
+
+    // index
+    mint operator[] (int k) const { return (k < poly.size()) ? poly[k] : 0; }
+
+    // istream
+    friend istream &operator>>(istream &is, Modulo_Polynomial &P) {
+        for (int i = 0; i < (int)P.poly.size(); i++) { is >> P.poly[i]; }
+        return (is);
+    }
+
+    // ostream
+    friend ostream &operator<<(ostream &os, const Modulo_Polynomial &P){
+        for (int i = 0; i < (int)P.poly.size(); i++){ os << (i ? " " : "") << P.poly[i]; }
+        return os;
+    }
+
+    // poly で保持しているベクトルの長さを size にする.
+    // size = -1 のときは, size = precision に変換される.
+    void resize(int size = -1) {
+        if (size == -1) { size = this -> precision; }
+        size = min(size, this -> precision);
+        poly.resize(size);
+    }
+
+    private:
+    int size() const { return poly.size(); }
+
+    void reduce() {
+        while (!poly.empty() && poly.back().is_zero()) { poly.pop_back(); }
+    }
+};
