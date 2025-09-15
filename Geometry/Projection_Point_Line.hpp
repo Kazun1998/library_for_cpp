7140923318445fca6e3dbfc06bba427c31afd872
Kazun1998k <hkazu1998@gmail.com>
2025-09-15 19:26:23 +0900

    直線の引数を L から l へ変更

--- Geometry/Projection_Point_Line.hpp
+++ Geometry/Projection_Point_Line.hpp
@@ -4,9 +4,9 @@
 
 namespace geometry {
     template<typename R>
-    Point<R> Projection(const Point<R> &P, const Line<R> &L) {
-        auto d = L.vectorize();
-        auto t = dot(P - L.A, d) / norm_2(d); 
-        return L.A + t * d;
+    Point<R> Projection(const Point<R> &P, const Line<R> &l) {
+        auto d = l.vectorize();
+        auto t = dot(P - l.A, d) / norm_2(d); 
+        return l.A + t * d;
     }
 }
