debe9c383685e0b90f71eabaeccaf55757c005ea
Kazun1998k <hkazu1998@gmail.com>
2025-09-15 19:44:39 +0900

    refactor: Reflection

--- Geometry/Refection_Point_Line.hpp
+++ Geometry/Refection_Point_Line.hpp
@@ -5,7 +5,7 @@
 namespace geometry {
     // 直線 L に関して, 点 P と対称な点
     template<typename R>
-    Point<R> Reflection(const Point<R> &P, const Line<R> &L) {
-        return P + 2 * (Projection(P, L) - P);
+    Point<R> Reflection(const Point<R> &P, const Line<R> &l) {
+        return 2 * Projection(P, l) - P;
     }
 }
