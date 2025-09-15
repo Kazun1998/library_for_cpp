b6f3731bd0364d871c18b1cc048697dea8d2d9f6
Kazun1998k <hkazu1998@gmail.com>
2025-09-15 13:41:07 +0900

    点の直線に関する対称移動 実装

---
+++ geometry/Line_Refection.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include"Line_Projection.hpp"
+
+namespace geometry {
+    // 直線 L に関して, 点 P と対称な点
+    template<typename R>
+    Point<R> Reflection(const Point<R> &P, const Line<R> &L) {
+        return P + 2 * (Projection(P, L) - P);
+    }
+}
