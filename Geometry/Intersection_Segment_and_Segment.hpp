ffac4c3257da3e65b68659e8f016c6232c5d1516
Kazun1998k <hkazu1998@gmail.com>
2025-09-19 23:58:39 +0900

    construct: has_Intersection (Segment ver)

---
+++ Geometry/Intersection_Segment_and_Segment.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include"Counter_Clockwise.hpp"
+#include"Segment.hpp"
+
+namespace geometry {
+    template<typename R>
+    bool has_Intersection(const Segment<R> &s, const Segment<R> &t) {
+        auto a = s.A, b = s.B, c = t.A, d = t.B;
+        return (Counter_Clockwise(a, b, c) * Counter_Clockwise(a, b, d) <= 0) && (Counter_Clockwise(c, d, a) * Counter_Clockwise(c, d, b) <= 0);
+    }
+}
