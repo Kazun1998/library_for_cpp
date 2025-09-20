53d35c50f852c610144be8147fe5c0dd594459d9
Kazun1998k <hkazu1998@gmail.com>
2025-09-20 18:09:25 +0900

    add: 点の分割

---
+++ Geometry/Division_Point.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include"Point.hpp"
+
+namespace geometry {
+    // 線分 AB を p:q に内分する点を求める.
+    template<typename R>
+    Point<R> Internal_Division(const Point<R> &A, const Point<R> &B, const R &p, const R &q) {
+        return (q * A + p * B) / (p + q);
+    }
+
+    // 線分 AB の中点
+    template<typename R>
+    Point<R> MidPoint(const Point<R> &A, const Point<R> &B) { return Internal_Division(A, B, 1, 1); }
+
+    // 線分 AB を p:q に外分する点を求める.
+    template<typename R>
+    Point<R> External_Division(const Point<R> &A, const Point<R> &B, const R &p, const R &q) {
+        return (-q * A + p * B) / (p - q);
+    }
+}
