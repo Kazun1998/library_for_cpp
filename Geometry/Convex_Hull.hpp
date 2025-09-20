c8af6be350d1b9bddd61fb7c09449de6726fdd07
Kazun1998k <hkazu1998@gmail.com>
2025-09-20 13:56:25 +0900

    add: Convex_Hull 実装

---
+++ Geometry/Convex_Hull.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include"Counter_Clockwise.hpp"
+#include"Polygon.hpp"
+
+namespace geometry {
+    template<typename R>
+    Polygon<R> Convex_Hull(vector<Point<R>> &points, bool strict = true) {
+        int n = points.size();
+        if (n <= 2) { return Polygon(points); }
+
+        sort(points.begin(), points.end(), compare_x<R>);
+
+        int k = 0;
+        vector<Point<R>> vertices(2 * n);
+        auto is_valid = [&](const int i) -> bool { 
+            int d = Counter_Clockwise(vertices[k - 2], vertices[k - 1], points[i]);
+            return (d == COUNTER_CLOCKWISE || (!strict && d == ONLINE_FRONT));
+        };
+
+        for (int i = 0; i < n; vertices[k++] = points[i++]) {
+            while(k >= 2 && !is_valid(i)) {
+                k--;
+            }
+        }
+
+        for (int i = n - 2, t = k + 1; i >= 0; vertices[k++] = points[i--]) {
+            while (k >= t && !is_valid(i)) {
+                k--;
+            }
+        }
+
+        vertices.resize(k - 1);
+        return Polygon(vertices);
+    }
+}
