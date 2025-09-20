4c1730b62c3f1cac4d92cbe227111927cc491882
Kazun1998k <hkazu1998@gmail.com>
2025-09-21 00:40:11 +0900

    feature: 所属判定実装

--- Geometry/Line.hpp
+++ Geometry/Line.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include"Point.hpp"
+#include"Counter_Clockwise.hpp"
 
 namespace geometry {
     template<typename R>
@@ -12,5 +13,10 @@ namespace geometry {
 
         inline Point<R> vectorize() const { return B - A; }
         inline Point<R> counter_vectorize() const { return A - B; }
+
+        Inclusion include(const Point<R> &P) {
+            int m = Counter_Clockwise(A, B, P);
+            return (m == ON_SEGMENT || m == ONLINE_BACK || m == ONLINE_FRONT) ? Inclusion::ON : Inclusion::OUT;
+        }
     };
 }
