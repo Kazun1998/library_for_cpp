2f54067979ff89847e99f94aa3bf5d8641d6c6fe
Kazun1998k <hkazu1998@gmail.com>
2025-09-20 15:21:53 +0900

    fix: 重複点に関するバグを修正

--- Geometry/Convex_Hull.hpp
+++ Geometry/Convex_Hull.hpp
@@ -5,11 +5,14 @@
 
 namespace geometry {
     template<typename R>
-    Polygon<R> Convex_Hull(vector<Point<R>> &points, bool strict = true) {
-        int n = points.size();
-        if (n <= 2) { return Polygon(points); }
-
+    Polygon<R> Convex_Hull(vector<Point<R>> points, bool strict = true) {
+        // 前処理
         sort(points.begin(), points.end(), compare_x<R>);
+        points.erase(unique(points.begin(), points.end()), points.end());
+
+        int n = points.size();
+
+        if (n <= 2) { return Polygon(points); }
 
         int k = 0;
         vector<Point<R>> vertices(2 * n);
@@ -19,12 +22,16 @@ namespace geometry {
         };
 
         for (int i = 0; i < n; vertices[k++] = points[i++]) {
+            if (!vertices.empty() && vertices.back() == points[i]) { vertices.pop_back(); }
+
             while(k >= 2 && !is_valid(i)) {
                 k--;
             }
         }
 
         for (int i = n - 2, t = k + 1; i >= 0; vertices[k++] = points[i--]) {
+            if (!vertices.empty() && vertices.back() == points[i]) { vertices.pop_back(); }
+
             while (k >= t && !is_valid(i)) {
                 k--;
             }
