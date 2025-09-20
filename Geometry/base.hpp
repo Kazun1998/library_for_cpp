1d05a9f6e5fb7e3e4afea5f994189042261dde74
Kazun1998k <hkazu1998@gmail.com>
2025-09-21 00:16:43 +0900

    add: Inclusion

--- Geometry/base.hpp
+++ Geometry/base.hpp
@@ -7,7 +7,7 @@ namespace geometry {
     const Real epsilon = 1e-9;
     const Real pi = acos(static_cast<Real>(-1));
 
-    enum { out = -1, on = 0, in = 1 };
+    enum class Inclusion { OUT = -1, ON = 0, IN = 1 };
     enum class Direction_Relation { PARALLEL = 1, ORTHOGONAL = -1, CROSS = 0}; 
 
     inline int sign(const Real &r) { return r <= -epsilon ? -1 : r >= epsilon ? 1: 0; }
