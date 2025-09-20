0dcf19d6416caf8c486f611477d4a000047f7c83
Kazun1998k <hkazu1998@gmail.com>
2025-09-20 16:51:29 +0900

    refactor

--- Geometry/Triangle.hpp
+++ Geometry/Triangle.hpp
@@ -16,10 +16,16 @@ namespace geometry {
         tuple<double, double, double> edges() const {
             return make_tuple(norm(C - B), norm(A - C), norm(B - A));
         }
+
+        // [PBC] : [PCA] : [PAB] = alpha : beta : gamma を満たす点 P を求める.
+        Point<R> balance(const R &alpha, const R &beta, const R &gamma) const {
+            return (alpha * A + beta * B + gamma * C) / (alpha + beta + gamma);
+        }
     };
 
     template<typename R>
     R Area(const Triangle<R> &T) {
-        return cross(T.B - T.A, T.C - T.A) / 2;
+        auto X = cross(T.B - T.A, T.C - T.A);
+        return sign(X) >= 0 ? X / 2 : -X / 2;
     }
 }
