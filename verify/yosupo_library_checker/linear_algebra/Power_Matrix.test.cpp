f9e9431e190415a39367f18c1bef19218c21e27e
Kazun1998k <hkazu1998@gmail.com>
2025-09-21 13:44:10 +0900

    add: verify of Power Matrix

---
+++ verify/yosupo_library_checker/linear_algebra/Power_Matrix.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_matrix"
+
+#include"../../../template/template.hpp"
+#include"../../../modint.hpp"
+#include"../../../Linear_Algebra/Field_Matrix.hpp"
+
+int main(){
+    int N; cin >> N;
+    ll K; cin >> K;
+    Field_Matrix<modint<998244353>> A(N);
+    cin >> A;
+    cout << power(A, K) << endl;
+}
