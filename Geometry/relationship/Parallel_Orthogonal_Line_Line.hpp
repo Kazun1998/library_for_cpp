fc445e8c62d58c620fedac42f7ccd2d43c06d4c8
Kazun1998k <hkazu1998@gmail.com>
2025-09-23 11:16:49 +0900

    add: is_Parallel と is_Orthogonal の実装

--- Geometry/relationship/Parallel_Orthogonal_Line_Line.hpp
+++ Geometry/relationship/Parallel_Orthogonal_Line_Line.hpp
@@ -4,10 +4,19 @@
 
 namespace geometry {
     template<typename R>
+    bool is_Parallel(const Line<R> &l, const Line<R> &m) {
+        return sign(dot(l.vectorize(), m.vectorize())) == 0;
+    }
+
+    template<typename R>
+    bool is_Orthogonal(const Line<R> &l, const Line<R> &m) {
+        return sign(cross(l.vectorize(), m.vectorize())) == 0;
+    }
+
+    template<typename R>
     Direction_Relation Parallel_Orthogonal (const Line<R> &l, const Line<R> &m) {
-        auto u = l.vectorize(), v = m.vectorize();
-        if (sign(dot(u, v)) == 0) { return Direction_Relation::ORTHOGONAL; }
-        if (sign(cross(u, v)) == 0) { return Direction_Relation::PARALLEL; }
+        if (is_Parallel(l, m)) { return Direction_Relation::ORTHOGONAL; }
+        if (is_Orthogonal(l, m)) { return Direction_Relation::PARALLEL; }
         return Direction_Relation::CROSS;
     }
 };
