6f09d027845395dea83a18386305748c70212657
Kazun1998k <hkazu1998@gmail.com>
2025-09-23 10:12:34 +0900

    refactor: 点の直線に関する対称移動を求める際に, 点の点に関する対称移動を経由

--- Geometry/Reflection_Point_Line.hpp
+++ Geometry/Reflection_Point_Line.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include"Projection_Point_Line.hpp"
+#include"Reflection_Point_Point.hpp"
 
 namespace geometry {
     // 直線 L に関して, 点 P と対称な点
     template<typename R>
     Point<R> Reflection(const Point<R> &P, const Line<R> &l) {
-        return 2 * Projection(P, l) - P;
+        return Reflection(P, Projection(P, l));
     }
 }
