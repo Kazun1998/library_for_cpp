3cfa3aa0031eceeb16fb6c2d0e81a501aca2efd7
Kazun1998k <hkazu1998@gmail.com>
2025-09-23 10:10:12 +0900

    fix: argument

--- Geometry/Reflection_Point_Point.hpp
+++ Geometry/Reflection_Point_Point.hpp
@@ -5,7 +5,7 @@
 namespace geometry {
     // 点 P が点 X に関して対称移動した後の点を求める.
     template<typename R>
-    Point<R> Reflection(const Point<R> &P, const Point<R> &A) {
-        return 2 * A - P;
+    Point<R> Reflection(const Point<R> &P, const Point<R> &X) {
+        return 2 * X - P;
     }
 }
