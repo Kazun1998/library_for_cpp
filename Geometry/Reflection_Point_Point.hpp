6d1c94783f53abc2c7779597f980b73e65dada5a
Kazun1998k <hkazu1998@gmail.com>
2025-09-23 10:07:29 +0900

    rename

--- Geometry/Reflection_Point_Point.hpp
+++ Geometry/Reflection_Point_Point.hpp
@@ -5,7 +5,7 @@
 namespace geometry {
     // 点 P が点 X に関して対称移動した後の点を求める.
     template<typename R>
-    Point<R> Projection(const Point<R> &P, const Point<R> &A) {
+    Point<R> Reflection(const Point<R> &P, const Point<R> &A) {
         return 2 * A - P;
     }
 }
