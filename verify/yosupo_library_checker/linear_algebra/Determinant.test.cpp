174c46beced7de56aaa81caa52ce420d4762f6f7
Kazun1998k <hkazu1998@gmail.com>
2025-09-24 00:56:33 +0900

    move: modint のファイルを移動

--- verify/yosupo_library_checker/linear_algebra/Determinant.test.cpp
+++ verify/yosupo_library_checker/linear_algebra/Determinant.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/matrix_det"
 
 #include"../../../template/template.hpp"
-#include"../../../modint.hpp"
+#include"../../../Algebra/modint.hpp"
 #include"../../../Linear_Algebra/Field_Matrix.hpp"
 
 int main(){
