9bbe715aae793159950d83ba9d8aaa64ebf47944
Kazun1998k <hkazu1998@gmail.com>
2025-10-08 00:45:48 +0900

    feature: Garner_Base

---
+++ Modulo/Garner.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include"Modulo.hpp"
+#include"Solve.hpp"
+
+namespace modulo {
+    vector<ll> Garner_Base(vector<Modulo> X) {
+        int n = X.size();
+        vector<ll> s(n);
+
+        for (int i = 0; i < n; i++) {
+            ll m = X[i].n;
+
+            ll a = 1;
+            for (int j = 0; j < i; j++) { a = mod(a * X[j].n, m); }
+
+            ll b = 0, h = 1;
+            for (int j = 0; j < i; j++) {
+                b = mod(b + s[j] * h, m);
+                h = mod(h * X[j].n, m);
+            }
+
+            s[i] = Solve_Congruence_Equation(a, X[i].a - b, m).a;
+        }
+
+        return s;
+    }
+}
