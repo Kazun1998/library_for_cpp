d6455d1a018b990073ebbe5395ccf6c459cf7281
Kazun1998k <hkazu1998@gmail.com>
2025-10-11 15:01:11 +0900

    feature: LIS

---
+++ Sequence/Longest_Increasing_Subsequence.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include"../template/template.hpp"
+
+template<typename O>
+struct Longest_Increasing_Subsequence {
+    int length = 0;
+    vector<O> lis;
+    vector<int> index;
+    
+    Longest_Increasing_Subsequence(const vector<O> &data, bool equal = false) {
+        int N = data.size();
+        vector<O> X;
+        vector<int> I(N);
+
+        for (int i = 0; i < N; i++) {
+            auto it = equal ? upper_bound(X.begin(), X.end(), data[i]) : lower_bound(X.begin(), X.end(), data[i]);
+            int k = distance(X.begin(), it);
+            if (k == X.size()) { X.emplace_back(data[i]); }
+            else { X[k] = data[i]; }
+
+            I[i] = k;
+        }
+
+        lis.clear(); index.clear();
+        for (int i = N - 1, j = X.size() - 1; i >= 0; i--) {
+            unless(I[i] == j) { continue; }
+
+            j--;
+            lis.emplace_back(data[i]);
+            index.emplace_back(i);
+            length++;
+        }
+
+        reverse(lis.begin(), lis.end());
+        reverse(index.begin(), index.end());
+    }
+};
