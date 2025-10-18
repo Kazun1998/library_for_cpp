c32e43d1998150ed2b7046215db489cda91c51e3
Kazun1998k <hkazu1998@gmail.com>
2025-10-18 12:03:24 +0900

    feature: group_count を実装し, 変数名を変更

--- Union_Find/Union_Find.hpp
+++ Union_Find/Union_Find.hpp
@@ -4,10 +4,10 @@ class Union_Find{
     private:
     int N;
     vector<int> data;
-    int group_number;
+    int _group_number;
 
     public:
-    Union_Find(int N) : N(N), data(N, -1), group_number(N) {}
+    Union_Find(int N) : N(N), data(N, -1), _group_number(N) {}
 
     // x が属する族の代表元を求める.
     int find(int x) { return data[x] < 0 ? x: data[x] = find(data[x]); }
@@ -16,7 +16,7 @@ class Union_Find{
     int add_vertex() {
       data.emplace_back(-1);
       N++;
-      group_number++;
+      _group_number++;
 
       return N - 1;
     }
@@ -43,7 +43,7 @@ class Union_Find{
         data[x] += data[y];
         data[y] = x;
 
-        group_number--;
+        _group_number--;
 
         return true;
     }
@@ -53,4 +53,7 @@ class Union_Find{
 
     // x, y は同じ連結成分にある?
     int same(int x, int y) { return find(x) == find(y); }
+
+    // 連結成分の数を求める.
+    inline int group_count() const { return _group_number; }
 };
