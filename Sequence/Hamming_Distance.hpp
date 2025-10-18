3f29a5341faf434338c675cf2e92ae430f7ccff4
Kazun1998k <hkazu1998@gmail.com>
2025-10-18 13:22:19 +0900

    string 版 Hamming Distance 作成

--- Sequence/Hamming_Distance.hpp
+++ Sequence/Hamming_Distance.hpp
@@ -15,3 +15,16 @@ size_t Hamming_Distance(const std::vector<T> &x, const std::vector<T> &y) {
 
     return distance;
 }
+
+size_t Hamming_Distance(const std::string &S, const std::string &T) {
+    if (S.size() != T.size()) {
+        throw std::invalid_argument("Strings must have the same size to calculate Hamming Distance.");
+    }
+
+    size_t distance = 0;
+    for (size_t i = 0; i < S.size(); i++) {
+        if (S[i] != T[i]) { distance++; }
+    }
+
+    return distance;
+}
