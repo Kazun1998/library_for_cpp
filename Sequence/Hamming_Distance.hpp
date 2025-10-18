58904ded6ff05185d7acfc4461b451ef8577dd8d
Kazun1998k <hkazu1998@gmail.com>
2025-10-18 13:24:06 +0900

    vector 版 Hamming Distance 修正

--- Sequence/Hamming_Distance.hpp
+++ Sequence/Hamming_Distance.hpp
@@ -3,14 +3,14 @@
 #include"../template/template.hpp"
 
 template<typename T>
-size_t Hamming_Distance(const std::vector<T> &x, const std::vector<T> &y) {
-    if (x.size() != y.size()) {
+size_t Hamming_Distance(const std::vector<T> &A, const std::vector<T> &B) {
+    if (A.size() != B.size()) {
         throw std::invalid_argument("Vectors must have the same size to calculate Hamming Distance.");
     }
 
     size_t distance = 0;
-    for (size_t i = 0; i < x.size(); i++) { 
-        if (x[i] != y[i]) { distance++; }
+    for (size_t i = 0; i < A.size(); i++) { 
+        if (A[i] != B[i]) { distance++; }
     }
 
     return distance;
