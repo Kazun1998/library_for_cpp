e5e60ea96078fd31e88a5300b8566a407bc8a24c
Kazun1998k <hkazu1998@gmail.com>
2025-10-26 00:37:41 +0900

    Combination_Calculator 実装

---
+++ Counting/Combination_Calculator.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include"../template/template.hpp"
+
+template<typename mint>
+class Combination_Calculator {
+    private:
+    vector<mint> _fact, _fact_inv, _inv;
+
+    void resize(const int m) {
+        if (m < _fact.size()) { return; }
+
+        for (int size = _fact.size(); m >= size; size = _fact.size()) {
+            _fact.resize(2 * size);
+            _fact_inv.resize(2 * size);
+            _inv.resize(2 * size);
+
+            for (int k = size; k < 2 * size; k++) {
+                _fact[k] = k * _fact[k - 1];
+            }
+
+            _fact_inv.back() = _fact.back().inverse();
+
+            for (int k = 2 * size - 2; k >= size; --k) {
+                _fact_inv[k] = (k + 1) * _fact_inv[k + 1];
+            }
+
+            for (int k = size; k < 2 * size; ++k) {
+                int q, r;
+                tie (q, r) = divmod(mint::mod(), k);
+                _inv[k] = -q * _inv[r];
+            }
+        }
+    }
+
+    public:
+    Combination_Calculator(const int n) {
+        _fact.emplace_back(1);
+        _fact_inv.emplace_back(1);
+        _inv.emplace_back(0);
+
+        resize(n);
+    }
+
+    Combination_Calculator(): Combination_Calculator(0) {}
+
+    mint fact(const int k) {
+        resize(k);
+        return _fact[k];
+    }
+
+    mint fact_inv(const int k) {
+        resize(k);
+        return _fact_inv[k];
+    }
+
+    mint inv(const int k) {
+        resize(k);
+        return _inv[k];
+    }
+};
