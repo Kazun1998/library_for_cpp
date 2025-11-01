0331719dbff5a2d8c81ee62e292c5354df1d4bc1
Kazun1998k <hkazu1998@gmail.com>
2025-11-01 19:59:12 +0900

    rename: from Prime_Count to Prime_Pi

--- verify/yosupo_library_checker/number_theory/Counting_Primes.test.cpp
+++ verify/yosupo_library_checker/number_theory/Counting_Primes.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/counting_primes"
 
 #include"../../../template/template.hpp"
-#include"../../../Integer/Prime_Count.hpp"
+#include"../../../Integer/Prime_Pi.hpp"
 
 int main() {
     ll N; cin >> N;
