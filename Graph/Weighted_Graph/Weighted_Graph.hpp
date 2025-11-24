cdc62f9b3bbddc9cb99a8e406113ad1ed2753491
Kazun1998k <hkazu1998@gmail.com>
2025-11-24 14:59:11 +0900

    incidence メソッドの返り値を修正

--- Graph/Weighted_Graph/Weighted_Graph.hpp
+++ Graph/Weighted_Graph/Weighted_Graph.hpp
@@ -48,8 +48,7 @@ namespace weighted_graph {
             return id;
         }
 
-        // 頂点 u に接続する辺 ID と隣接頂点 v のペア { ID, v } のリストを取得
-        inline const vector<pair<int, int>>& incidence (int u) const { return incidences[u]; }
+        inline const vector<Edge>& incidence (int u) const { return incidences[u]; }
 
         /// @brief 辺 ID が id である辺を取得する.
         /// @param id
