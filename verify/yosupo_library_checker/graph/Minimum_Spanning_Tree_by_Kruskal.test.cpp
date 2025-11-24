56d226567093b5d4abf371d7816fa55755d161c0
Kazun1998k <hkazu1998@gmail.com>
2025-11-24 14:50:40 +0900

    add: verify of Kruskal

---
+++ verify/yosupo_library_checker/graph/Minimum_Spanning_Tree_by_Kruskal.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/minimum_spanning_tree"
+
+#include"../../../template/template.hpp"
+#include"../../../Graph/Weighted_Graph/Kruskal.hpp"
+
+using namespace weighted_graph;
+
+int main() {
+    int N, M; cin >> N >> M;
+    Weighted_Graph<ll> G(N);
+
+    for (int j = 0; j < M; j++) {
+        int a, b; ll c;
+        scanf("%d%d%lld", &a, &b, &c);
+        G.add_edge(a, b, c);
+    }
+
+    Minimum_Spanning_Tree T = Kruskal(G);
+
+    cout << T.weight << endl;
+    for (int k = 0; k < T.edges.size(); k++) {
+        cout << (k ? " " : "") << T.edges[k].id;
+    }
+
+    cout << "\n";
+}
