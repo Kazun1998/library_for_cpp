4e8cf9a41f3d393e80d537e0ff2958412e87eb73
Kazun1998k <hkazu1998@gmail.com>
2025-12-24 00:41:54 +0900

    区間和を区間積に改名

--- Math/Interval_Product.hpp
+++ Math/Interval_Product.hpp
@@ -2,7 +2,7 @@
 
 #include"../template/template.hpp"
 
-namespace interval_sum {
+namespace interval_product {
     template <typename F, typename G>
     concept Binary_Operator = requires(F f, G g) {
         { f(g, g) } -> std::convertible_to<G>;
@@ -15,24 +15,24 @@ namespace interval_sum {
 
     template<class G, auto op, auto inv, auto identity>
     requires Binary_Operator<decltype(op), G> &&  Unary_Operator<decltype(inv), G> && convertible_to<decltype(identity), G>
-    class Interval_Sum {
-        vector<G> prefix_sum;
+    class Interval_Product {
+        vector<G> prefix_prod;
 
         public:
-        Interval_Sum(const vector<G> &data) {
-            prefix_sum.reserve(data.size() + 1);
+        Interval_Product(const vector<G> &data) {
+            prefix_prod.reserve(data.size() + 1);
 
-            // 累積和は exclusive でとる. つまり, prefix_sum[i] = data[0] * data[1] * ... * data[i - 1] になる.
-            prefix_sum.push_back(identity);
+            // 累積和は exclusive でとる. つまり, prefix_prod[i] = data[0] * data[1] * ... * data[i - 1] になる.
+            prefix_prod.push_back(identity);
             for (const G &x : data) {
-                prefix_sum.push_back(op(prefix_sum.back(), x));
+                prefix_prod.push_back(op(prefix_prod.back(), x));
             }
         }
 
         /// @brief 閉区間 [0, r] における累積
         /// @param r 区間の右端
         /// @return 
-        inline G query(const int r) const { return prefix_sum[r + 1]; }
+        inline G query(const int r) const { return prefix_prod[r + 1]; }
 
         /// @brief 閉区間 [l, r] における累積
         /// @param l 区間の左端
