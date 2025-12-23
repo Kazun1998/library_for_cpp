50947e0fb967d4b89a9f34659c734291c0bf3989
Kazun1998k <hkazu1998@gmail.com>
2025-12-24 00:42:00 +0900

    fix: verify

--- verify/yosupo_library_checker/data_structure/Static_Range_Sum-2.test.cpp
+++ verify/yosupo_library_checker/data_structure/Static_Range_Sum-2.test.cpp
@@ -1,14 +1,14 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
 
 #include"../../../template/template.hpp"
-#include"../../../Math/Interval_Sum.hpp"
+#include"../../../Math/Interval_Product.hpp"
 
 int main() {
     int N, Q; cin >> N >> Q;
     vector<ll> a(N);
     for (int i = 0; i < N; i++) scanf("%lld", &a[i]);
 
-    interval_sum::Interval_Sum<ll, add<ll>, neg<ll>, 0LL> I(a);
+    interval_product::Interval_Product<ll, add<ll>, neg<ll>, 0LL> I(a);
 
     for (int q = 1; q <= Q; q++) {
         int l, r; scanf("%d%d", &l, &r);
