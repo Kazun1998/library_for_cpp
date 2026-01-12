6a0e24ffe95198ec1662521a58af5572f38a05a5
Kazun1998k <hkazu1998@gmail.com>
2026-01-12 22:30:37 +0900

    Solve 実装

--- Math/Bezout.hpp
+++ Math/Bezout.hpp
@@ -32,4 +32,50 @@ namespace bezout {
         auto [p, q, g] = Extended_Euclid<T>(a, b);
         return Find_Particular_Solution<T>(a, b, c, {p, q});
     }
+
+    template<integral T>
+    tuple<T, T, T, T, T, T> Solve(T a, T b, T c, const T lx, const T rx, const T ly, const T ry, const pair<T, T> &hint) {
+        T p, q;
+        tie (p, q) = hint;
+        const T g = a * p + b * q;
+
+        if (safe_mod(c, g) != 0) { throw NotExist(); }
+
+        a /= g; b /= g; c /= g;
+
+        int tmp_l, tmp_r;
+        if (b > 0) {
+            tmp_l = lx - c * p;
+            tmp_r = rx - c * p;
+        } else {
+            tmp_l = -(rx - c * p);
+            tmp_r = -(lx - c * p);
+        }
+
+        T klx = div_ceil(tmp_l, abs<T>(b));
+        T krx = div_floor(tmp_r, abs<T>(b));
+
+        if (a > 0) {
+            tmp_l = -ry + c * q;
+            tmp_r = -ly + c * q;
+        } else {
+            tmp_l = -(-ly + c * q);
+            tmp_r = -(-ry + c * q);
+        }
+
+        T kly = div_ceil(tmp_l, abs<T>(a));
+        T kry = div_floor(tmp_r, abs<T>(a));
+
+        T kl = max<T>(klx, kly), kr = min<T>(krx, kry);
+
+        if (kl > kr) throw NotExist();
+
+        return { c * p, b, c * q, -a, kl, kr };
+    };
+
+    template<integral T>
+    tuple<T, T, T, T, T, T> Solve(T a, T b, T c, const T lx, const T rx, const T ly, const T ry) {
+        auto [p, q, g] = Extended_Euclid<T>(a, b);
+        return Solve<T>(a, b, c, lx, rx, ly, ry, make_pair(p, q));
+    };
 }
