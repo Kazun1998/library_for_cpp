1b76ad8f9d756036ccd6eb2873051b468e9f2319
Kazun1998k <hkazu1998@gmail.com>
2026-01-24 02:00:02 +0900

    解が存在しない場合は std::nullopt を返すように

--- Math/Bezout.hpp
+++ Math/Bezout.hpp
@@ -6,40 +6,40 @@
 namespace bezout {
     /// @brief 1次不定方程式 a x + b y = c の解 (x, y) を 1 組見つける.
     /// @param hint (p, q) は a p + b q = gcd(a, b) を満たす.
-    /// @return 解 (x, y)
+    /// @return 解 (x, y). 解が存在しない場合は std::nullopt.
     template<integral T>
-    pair<T, T> Find_Particular_Solution(const T a, const T b, const T c, const pair<T, T> &hint) {
+    optional<pair<T, T>> Find_Particular_Solution(const T a, const T b, const T c, const pair<T, T> &hint) {
         T p, q;
         tie (p, q) = hint;
 
         const T g = a * p + b * q;
 
         if (g == 0) {
-            if (c == 0) { return {0, 0}; }
-            throw NotExist();
+            if (c == 0) { return pair<T, T>(0, 0); }
+            return nullopt;
         }
 
-        if (safe_mod(c, g) != 0) { throw NotExist(); }
+        if (safe_mod(c, g) != 0) { return nullopt; }
 
         const T k = c / g;
-        return {p * k, q * k};
+        return pair<T, T>(p * k, q * k);
     }
 
     /// @brief 1次不定方程式 a x + b y = c の解 (x, y) を 1 組見つける.
-    /// @return 解 (x, y)
+    /// @return 解 (x, y). 解が存在しない場合は std::nullopt.
     template<integral T>
-    pair<T, T> Find_Particular_Solution(const T a, const T b, const T c) {
+    optional<pair<T, T>> Find_Particular_Solution(const T a, const T b, const T c) {
         auto [p, q, g] = Extended_Euclid<T>(a, b);
         return Find_Particular_Solution<T>(a, b, c, {p, q});
     }
 
     template<integral T>
-    tuple<T, T, T, T, T, T> Solve(T a, T b, T c, const T lx, const T rx, const T ly, const T ry, const pair<T, T> &hint) {
+    optional<tuple<T, T, T, T, T, T>> Solve(T a, T b, T c, const T lx, const T rx, const T ly, const T ry, const pair<T, T> &hint) {
         T p, q;
         tie (p, q) = hint;
         const T g = a * p + b * q;
 
-        if (safe_mod(c, g) != 0) { throw NotExist(); }
+        if (safe_mod(c, g) != 0) { return nullopt; }
 
         a /= g; b /= g; c /= g;
 
@@ -68,13 +68,13 @@ namespace bezout {
 
         T kl = max<T>(klx, kly), kr = min<T>(krx, kry);
 
-        if (kl > kr) throw NotExist();
+        if (kl > kr) return nullopt;
 
-        return { c * p, b, c * q, -a, kl, kr };
+        return make_tuple(c * p, b, c * q, -a, kl, kr);
     };
 
     template<integral T>
-    tuple<T, T, T, T, T, T> Solve(T a, T b, T c, const T lx, const T rx, const T ly, const T ry) {
+    optional<tuple<T, T, T, T, T, T>> Solve(T a, T b, T c, const T lx, const T rx, const T ly, const T ry) {
         auto [p, q, g] = Extended_Euclid<T>(a, b);
         return Solve<T>(a, b, c, lx, rx, ly, ry, make_pair(p, q));
     };
