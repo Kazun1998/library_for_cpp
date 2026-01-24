f7d36856ada0f1cbe82bceab5a789d08f6af0035
Kazun1998k <hkazu1998@gmail.com>
2026-01-24 12:37:13 +0900

    concepts を include

--- template/template.hpp
+++ template/template.hpp
@@ -17,6 +17,7 @@ using namespace std;
 #include <climits>
 #include <cmath>
 #include <complex>
+#include <concepts>
 #include <cstdarg>
 #include <cstddef>
 #include <cstdint>
@@ -39,6 +40,7 @@ using namespace std;
 #include <new>
 #include <numeric>
 #include <ostream>
+#include <optional>
 #include <queue>
 #include <random>
 #include <set>
