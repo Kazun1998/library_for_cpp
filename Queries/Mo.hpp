d313fea2c8426837cb751be29ec68aed273dd3e9
Kazun1998k <hkazu1998@gmail.com>
2026-01-24 20:25:43 +0900

    Mo 実装

---
+++ Queries/Mo.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+class Mo {
+    private:
+    int N;
+    int query_count;
+    int l, r;
+    vector<int> left, right;
+
+    template<typename ADD, typename DEL>
+    void move(const int tl, const int tr, const ADD &add, const DEL &del) {
+        while (tl < l) add(--l);
+        while (r < tr) add(r++);
+
+        while(l < tl) del(l++);
+        while(tr < r) del(--r);
+    }
+
+    public:
+    Mo(const int N): N(N), query_count(0), left(0), right(0) {}
+
+    /// @brief 閉区間 [l, r] に関するクエリを追加する
+    /// @param l 左端
+    /// @param r 右端
+    void add_query(const int l, const int r) {
+        left.emplace_back(l);
+        right.emplace_back(r + 1);
+        query_count++;
+    }
+
+    template<typename ADD, typename DEL, typename REM>
+    void run(const ADD &add, const DEL &del, const REM &rem) {
+        int bucket_size = max<int>(1, 1.0 * N / max<double>(1.0, sqrt(query_count * 2.0 / 3.0)));
+        int bucket_count = div_ceil(N, bucket_size);
+        vector<vector<int>> buckets(bucket_count, vector<int>(0));
+
+        for (int q = 0; q < query_count; q++) {
+            int bucket_id = div_floor(left[q], bucket_size);
+            buckets[bucket_id].emplace_back(q);
+        }
+
+        l = 0, r = 0;
+        for (int bucket_id = 0; bucket_id < bucket_count; bucket_id++) {
+            auto &bucket = buckets[bucket_id];
+            if (bucket_id % 2 == 0) {
+                sort(bucket.begin(), bucket.end(), [&](const int p, const int q) -> bool { return right[p] < right[q]; });
+            } else {
+                sort(bucket.rbegin(), bucket.rend(), [&](const int p, const int q) -> bool { return right[p] < right[q]; });
+            }
+
+            for (const int q: bucket) {
+                move(left[q], right[q], add, del);
+                rem(q);
+            }
+        }
+    }
+};
