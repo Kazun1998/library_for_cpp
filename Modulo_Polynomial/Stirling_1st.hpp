bc99f9ebe1b5ec8c5202263971546a6b82e3774d
Kazun1998k <hkazu1998@gmail.com>
2026-02-08 02:21:02 +0900

    add comment of Stirling_1st

--- Modulo_Polynomial/Stirling_1st.hpp
+++ Modulo_Polynomial/Stirling_1st.hpp
@@ -3,6 +3,12 @@
 #include"Fast_Power_Series.hpp"
 #include"Taylor_Shift.hpp"
 
+/// @brief 第 I 種スターリング数 (Stirling numbers of the first kind) を計算する
+/// @tparam mint
+/// @param N 
+/// @param sign 符号の有無. false の場合は符号なし (上昇階乗冪の係数), true の場合は符号あり (下降階乗冪の係数)
+/// @return 長さ N + 1 の vector. i 番目の要素は S_1(N, i)
+/// @note 計算量 O(N log N)
 template<typename mint>
 vector<mint> Stirling_1st(int N, bool sign = false) {
     using FPS = Fast_Power_Series<mint>;
