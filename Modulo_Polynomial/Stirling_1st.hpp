278c1ec09666ba05731774971da1236da08c9afe
Kazun1998k <hkazu1998@gmail.com>
2026-02-08 02:19:28 +0900

    Stirling_1st の高速化

--- Modulo_Polynomial/Stirling_1st.hpp
+++ Modulo_Polynomial/Stirling_1st.hpp
@@ -6,15 +6,23 @@
 template<typename mint>
 vector<mint> Stirling_1st(int N, bool sign = false) {
     using FPS = Fast_Power_Series<mint>;
+    if (N == 0) { return {1}; }
 
-    auto g = [&N](auto self, int n) -> FPS {
-        if (n == 0) { return FPS({1}, N + 1); }
-        if (n == 1) { return FPS({0, 1}, N + 1); }
+    auto g = [](auto self, int n) -> FPS {
+        if (n == 0) { return FPS({1}, 1); }
+        if (n == 1) { return FPS({0, 1}, 2); }
 
-        if (n % 2 == 1) { return self(self, n - 1) * FPS({n - 1, 1}, N + 1); }
+        if (n % 2 == 1) {
+            FPS P = self(self, n - 1);
+            P.update_precision(n + 1);
+            return P * FPS({n - 1, 1}, n + 1);
+        }
 
         FPS P = self(self, n / 2);
-        return P * Taylor_Shift(P, n / 2);
+        FPS Q = Taylor_Shift(P, n / 2);
+        P.update_precision(n + 1);
+        Q.update_precision(n + 1);
+        return P * Q;
     };
     
     auto c = g(g, N).poly;
@@ -22,7 +30,7 @@ vector<mint> Stirling_1st(int N, bool sign = false) {
     unless(sign) { return c; }
 
     for (int k = 0; k <= N; k++) {
-        unless (safe_mod(k, 2) == safe_mod(N, 2)) { c[k] *= -1; }
+        unless (k % 2 == N % 2) { c[k] *= -1; }
     }
 
     return c;
