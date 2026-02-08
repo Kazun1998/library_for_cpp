9c0194233f9886ef832a4e3d1457a916f4805d9a
Kazun1998k <hkazu1998@gmail.com>
2026-02-08 10:45:05 +0900

    Revert "FPS を経由しない形式に変更"
    This reverts commit 2e405ad9d0e25758df0aa71073f74924316d7896.

--- Modulo_Polynomial/Stirling_1st.hpp
+++ Modulo_Polynomial/Stirling_1st.hpp
@@ -11,27 +11,27 @@
 /// @note 計算量 O(N log N)
 template<typename mint>
 vector<mint> Stirling_1st(int N, bool sign = false) {
+    using FPS = Fast_Power_Series<mint>;
     if (N == 0) { return {1}; }
 
-    Numeric_Theory_Translation<mint> ntt;
-
-    auto g = [&](auto self, int n) -> vector<mint> {
-        if (n == 0) { return {1}; }
-        if (n == 1) { return {0, 1}; }
+    auto g = [](auto self, int n) -> FPS {
+        if (n == 0) { return FPS({1}, 1); }
+        if (n == 1) { return FPS({0, 1}, 2); }
 
         if (n % 2 == 1) {
-            vector<mint> P = self(self, n - 1);
-            vector<mint> Q = {mint(n - 1), 1};
-            return ntt.convolution(P, Q);
+            FPS P = self(self, n - 1);
+            P.update_precision(n + 1);
+            return P * FPS({n - 1, 1}, n + 1);
         }
 
-        vector<mint> P = self(self, n / 2);
-        Fast_Power_Series<mint> P_fps(P);
-        Fast_Power_Series<mint> Q_fps = Taylor_Shift(P_fps, mint(n / 2));
-        return ntt.convolution(P, Q_fps.poly);
+        FPS P = self(self, n / 2);
+        FPS Q = Taylor_Shift(P, n / 2);
+        P.update_precision(n + 1);
+        Q.update_precision(n + 1);
+        return P * Q;
     };
     
-    auto c = g(g, N);
+    auto c = g(g, N).poly;
 
     unless(sign) { return c; }
 
