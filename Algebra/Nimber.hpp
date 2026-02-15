dfd7812487d8f020f62148a8e288986aae30e419
Kazun1998k <hkazu1998@gmail.com>
2026-02-15 10:29:30 +0900

    積の高速化

--- Algebra/Nimber.hpp
+++ Algebra/Nimber.hpp
@@ -25,6 +25,11 @@ class Nimber {
 
     // 乗法
     Nimber& operator*=(const Nimber &b){
+        if ((x | b.x) < 256) {
+            if (!table_initialized) init_table();
+            x = small_table[x][b.x];
+            return *this;
+        }
         int level = max(calculate_level(x), calculate_level(b.x));
         x = calculate_mul(x, b.x, level);
         return *this;
@@ -45,9 +50,37 @@ class Nimber {
     private:
     uint64_t x;
 
+    inline static uint8_t small_table[256][256];
+    inline static bool table_initialized = false;
+
+    static void init_table() {
+        if (table_initialized) return;
+        small_table[0][0] = 0; small_table[0][1] = 0;
+        small_table[1][0] = 0; small_table[1][1] = 1;
+        for (int level = 1; level <= 3; ++level) {
+            int half = 1 << (1 << (level - 1));
+            int full = 1 << (1 << level);
+            for (int i = 0; i < full; ++i) {
+                for (int j = 0; j < full; ++j) {
+                    if (i < half && j < half) continue;
+                    int x0 = i & (half - 1);
+                    int x1 = i >> (1 << (level - 1));
+                    int y0 = j & (half - 1);
+                    int y1 = j >> (1 << (level - 1));
+                    int p = small_table[x0][y0];
+                    int m = small_table[x0 ^ x1][y0 ^ y1];
+                    int b = small_table[small_table[x1][y1]][half >> 1];
+                    small_table[i][j] = p ^ b ^ ((p ^ m) << (1 << (level - 1)));
+                }
+            }
+        }
+        table_initialized = true;
+    }
+
     static uint64_t calculate_mul(const uint64_t x, const uint64_t y, int level) {
-        if (level == 0) { 
-            return (x == 1 && y == 1) ? 1 : 0;
+        if (level <= 3) { 
+            if (!table_initialized) init_table();
+            return small_table[x][y];
         }
 
         const auto &[x1, x0] = separate(x, level);
