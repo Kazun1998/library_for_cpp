4dd78d0159397315b034d39d7cc2c4fa388eaedd
Kazun1998k <hkazu1998@gmail.com>
2026-02-23 17:37:02 +0900

    コンストラクタ実装

--- Min_Cost_Flow/Bipartite_Weighted_Matching.hpp
+++ Min_Cost_Flow/Bipartite_Weighted_Matching.hpp
@@ -5,5 +5,10 @@
 namespace min_cost_flow {
     template<class Weight>
     class Bipartite_Weighted_Matching {
+        public:
+        Bipartite_Weighted_Matching(int m, int n): m(m), n(n) {}
+
+        private:
+        int m, n;
     };
 }
