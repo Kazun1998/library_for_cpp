01f5d125b3d566aeefd8891752b30b74b75c7b71
Kazun1998k <hkazu1998@gmail.com>
2026-02-23 17:38:44 +0900

    add_edge 実装

--- Min_Cost_Flow/Bipartite_Weighted_Matching.hpp
+++ Min_Cost_Flow/Bipartite_Weighted_Matching.hpp
@@ -6,9 +6,14 @@ namespace min_cost_flow {
     template<class Weight>
     class Bipartite_Weighted_Matching {
         public:
-        Bipartite_Weighted_Matching(int m, int n): m(m), n(n) {}
+        Bipartite_Weighted_Matching(int m, int n): m(m), n(n), edges(m, vector<pair<int, Weight>>{}) {}
+
+        void add_edge(int a, int b, Weight w) {
+            edges[a].emplace_back(b, w);
+        }
 
         private:
         int m, n;
+        vector<vector<pair<int, Weight>>> edges;
     };
 }
