0377a825a4e2ab3c2470324a5dbe952bb4a3a288
Kazun1998k <hkazu1998@gmail.com>
2026-02-28 00:14:05 +0900

    sum_sq 実装

--- Math/Summation.hpp
+++ Math/Summation.hpp
@@ -46,4 +46,10 @@ namespace summation {
     T clamp_linear(T a, T b, T min_val, T max_val, T l, T r) {
         return linear(a, b, l, r) - relu_linear(a, b - max_val, l, r) + relu_linear(-a, min_val - b, l, r);
     }
+
+    template<typename T>
+    T sum_sq(T n) {
+        if (n < 0) return 0;
+        return n * (n + 1) * (2 * n + 1) / 6;
+    }
 }
