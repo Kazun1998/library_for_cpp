77423f07711100966498adb34267da9f974ca0a5
Kazun1998k <hkazu1998@gmail.com>
2026-03-07 23:22:52 +0900

    インスタンス変数の実装

--- Math/Odd_Montgomery_Multiplication.hpp
+++ Math/Odd_Montgomery_Multiplication.hpp
@@ -4,4 +4,13 @@
 struct Odd_Montgomery_Multiplication {
     using u64 = uint64_t;
     using u128 = __uint128_t;
+
+    /// @brief 剰余
+    u64 mod;
+
+    /// @brief N = mod としたとき, NN' ≡ -1 (mod R) を満たす N'
+    u64 neg_inv;
+
+    /// @brief (2^64)^2 mod N
+    u64 r2;
 };
