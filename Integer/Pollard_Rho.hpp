30d33ddcea676768691f8458e84e16cfc28df214
Kazun1998k <hkazu1998@gmail.com>
2026-03-12 23:18:08 +0900

    Find_Prime_Factor_by_Pollard_Rho 高速化

--- Integer/Pollard_Rho.hpp
+++ Integer/Pollard_Rho.hpp
@@ -21,21 +21,42 @@ uint64_t Find_Prime_Factor_by_Pollard_Rho(uint64_t n) {
             return z;
         };
         
+        // form で変換した後の整数で gcd を取っているが,
+        // n が奇数で, form ではOdd_Montgomery_Multiplication の取り方的に n と互いに素な2^64 をかけて n で割っているため,
+        // gcd は不変である.
+
         uint64_t x = calc.form(0), y = calc.form(0);
         uint64_t g = 1;
+        uint64_t q = calc.form(1);
+        uint64_t m = 500;
+
         while (g == 1) {
-            x = f(x);
-            y = f(f(y));
+            uint64_t xs = x, ys = y;
+            for (int i = 0; i < m; ++i) {
+                x = f(x);
+                y = f(f(y));
+                uint64_t diff = x < y ? y - x : x - y;
+                q = calc.multiply(q, diff);
+            }
+            g = gcd(q, n);
+            if (g == 1) continue;
+            if (g == n) {
+                g = 1;
+                x = xs; y = ys;
+                while (g == 1) {
+                    x = f(x);
+                    y = f(f(y));
+                    uint64_t diff = x < y ? y - x : x - y;
+                    g = gcd(diff, n);
+                }
+                if (g == n) break;
+            }
 
-            uint64_t diff = x < y ? y - x : x - y;
-            g = gcd(diff, n);
+            if (Miller_Rabin_Primality_Test(g)) return g;
+            if (Miller_Rabin_Primality_Test(n / g)) return n / g;
+            return Find_Prime_Factor_by_Pollard_Rho(g);
         }
-
-        if (g == n) continue;
-        
-        return Find_Prime_Factor_by_Pollard_Rho(g);
     }
-
     return n;
 }
 
