6c5686b86852365fe86267889b9519fabdd88db2
Kazun1998k <hkazu1998@gmail.com>
2026-03-15 12:56:46 +0900

    First_Garner を一般の場合にも対応

--- Modulo/Garner.hpp
+++ Modulo/Garner.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "../template/template.hpp"
+#include "../Integer/Prime.hpp"
 #include "Modulo.hpp"
 
 namespace modulo {
-    vector<ll> Garner(const vector<Modulo> &X) {
+    vector<ll> Garner_Base(const vector<Modulo> &X) {
         int n = X.size();
         vector<ll> a(n), m(n);
         for (int i = 0; i < n; ++i) {
@@ -16,7 +17,7 @@ namespace modulo {
             ll m_product = 1;
             ll left = 0;
             for (int j = 0; j < i; ++j) {
-                left += safe_mod(m_product * s[j], m[i]);
+                left = safe_mod(left + safe_mod(m_product * s[j], m[i]), m[i]);
                 m_product = safe_mod(m_product * m[j], m[i]);
             }
 
@@ -27,9 +28,46 @@ namespace modulo {
         return s;
     }
 
-    ll First_Garner(const vector<Modulo> &X, const ll R) {
+    optional<ll> First_Garner(vector<Modulo> X, const ll R, bool positive = false) {
         int n = X.size();
-        auto s = Garner(X);
+
+        map<ll, pair<int, int>> champions;
+        prime::Pseudo_Prime_Generator generator;
+
+        for (int i = 0; i < n; ++i) {
+            for (auto [p, e]: prime::prime_factorization(X[i].n)) {
+
+                if (champions.count(p) == 0) {
+                    champions[p] = {i, e};
+                    continue;
+                }
+
+                auto [j, f] = champions[p];
+
+                ll q = intpow(p, min<int>(e, f));
+
+                unless (X[i].a % q == X[j].a % q) return nullopt;
+
+                if (e >= f) {
+                    X[j].degenerate(X[j].n / q);
+                    champions[p] = {i, e};
+                }
+                else {
+                    X[i].degenerate(X[i].n / q);
+                }
+            }
+        }
+
+        auto s = Garner_Base(X);
+
+        if (positive && ranges::count(s, 0) == n) {
+            ll res = 1;
+            for (const auto &x: X) {
+                res = safe_mod(res * x.n, R);
+            }
+
+            return res;
+        }
 
         ll res = 0;
         for (int i = n - 1; i >=0 ; --i) {
