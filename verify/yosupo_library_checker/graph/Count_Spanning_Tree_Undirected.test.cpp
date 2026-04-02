832994d53e857d365d387ac915c333d399776696
Kazun1998k <hkazu1998@gmail.com>
2026-04-02 09:20:14 +0900

    add verify of Count_Spanning_Tree

---
+++ verify/yosupo_library_checker/graph/Count_Spanning_Tree_Undirected.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/counting_spanning_tree_undirected"
+
+#include "../../../template/template.hpp"
+#include "../../../Graph/Graph/Count_Spanning_Tree.hpp"
+#include "../../../Algebra/modint.hpp"
+
+using namespace graph;
+using mint = modint<998244353>;
+
+mint verify() {
+    int N, M;
+    cin >> N >> M;
+    Graph G(N);
+    for (int j = 0; j < M; ++j) {
+        int u, v; cin >> u >> v;
+        G.add_edge(u, v);
+    }
+    return Count_Spanning_Tree<mint>(G);
+}
+
+int main() {
+    cout << verify() << endl;
+}
