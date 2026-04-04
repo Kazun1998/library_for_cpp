f77ef3b5ab7c6ced70cf0afce4305bbfae092726
Kazun1998k <hkazu1998@gmail.com>
2026-04-05 00:45:58 +0900

    add verify of Subset_Convolution

---
+++ verify/yosupo_library_checker/subset_power_series/subset_convolution.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/subset_convolution"
+
+#include"../../../template/template.hpp"
+#include"../../../Algebra/modint.hpp"
+#include"../../../Convolution/Subset_Convolution.hpp"
+
+using mint = modint<998244353>;
+
+int main() {
+    int N; cin >> N;
+    convolution::Sub<mint> A(N + 1), B(N + 1);
+    for (int i = 1; i <= N; i++) scanf("%d", &A[i]);
+    for (int j = 1; j <= N; j++) scanf("%d", &B[j]);
+
+    vector<mint> c = (A * B).to_vector();
+    c.erase(c.begin(), c.begin() + 1);
+    cout << c << endl;
+}
