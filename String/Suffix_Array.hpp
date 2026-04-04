592ae8d52a5870e21507a6ec67bcb4bf37179f06
Kazun1998k <hkazu1998@gmail.com>
2026-04-05 01:25:37 +0900

    Suffix_Array (O(N (log N)^2) ver) 実装

---
+++ String/Suffix_Array.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+template<integral T>
+vector<int> Suffix_Array(const vector<T> &A) {
+    int n = A.size();
+    vector<int> suffix_array(n + 1);
+    vector<int> rank(n + 1);
+
+    iota(suffix_array.begin(), suffix_array.end(), 0);
+    for (int i = 0; i <= n; ++i) rank[i] = (i < n) ? int(A[i]) : -1;
+
+    for (int k = 1; k <= n; k <<= 1) {
+        auto compare = [&](int i, int j) {
+            unless (rank[i] == rank[j]) return rank[i] < rank[j];
+
+            int ri = (i + k <= n) ? rank[i + k] : -1;
+            int rj = (j + k <= n) ? rank[j + k] : -1;
+
+            return ri < rj;
+        };
+
+        sort(suffix_array.begin(), suffix_array.end(), compare);
+
+        vector<int> next_rank(n + 1);
+        next_rank[suffix_array[0]] = 0;
+        for (int i = 1; i <= n; ++i) {
+            next_rank[suffix_array[i]] = next_rank[suffix_array[i - 1]] + (compare(suffix_array[i - 1], suffix_array[i]) ? 1 : 0);
+        }
+
+        rank = move(next_rank);
+
+        if (rank[suffix_array[n]] == n) break;
+    }
+
+    suffix_array.erase(suffix_array.begin());
+    return suffix_array;
+}
+
+vector<int> Suffix_Array(const string &S) {
+    return Suffix_Array<char>(vector<char>(S.begin(), S.end()));
+}
