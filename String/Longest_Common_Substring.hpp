19bcdd9a40de2933a5b7e2c378dda002d6e100de
Kazun1998k <hkazu1998@gmail.com>
2026-04-05 19:25:06 +0900

    add comment

--- String/Longest_Common_Substring.hpp
+++ String/Longest_Common_Substring.hpp
@@ -2,6 +2,17 @@
 
 #include "Longest_Common_Prefix.hpp"
 
+/**
+ * @brief 2つの配列の最長共通連続部分列（LCS）を求める.
+ * 
+ * 接尾辞配列（Suffix Array）と LCP 配列を用いて O((N+M) log (N+M)) で計算する.
+ * 
+ * @tparam T 配列の要素の型. std::totally_ordered を満たす必要がある.
+ * @param A 1つ目の配列
+ * @param B 2つ目の配列
+ * @return tuple<int, int, int, int> {A内の開始位置, A内の終了位置(inclusive), B内の開始位置, B内の終了位置(inclusive)}
+ *         共通部分がない場合は {0, -1, 0, -1} を返す.
+ */
 template<totally_ordered T>
 tuple<int, int, int, int> Longest_Common_Continuous_Subsequence(const vector<T> &A, const vector<T> &B) {
     int n = A.size(), m = B.size();
@@ -56,6 +67,13 @@ tuple<int, int, int, int> Longest_Common_Continuous_Subsequence(const vector<T> 
     return {a, b, c, d};
 }
 
+/**
+ * @brief 2つの文字列の最長共通部分文字列（LCS）を求める.
+ * 
+ * @param S 1つ目の文字列
+ * @param T 2つ目の文字列
+ * @return tuple<int, int, int, int> {S内の開始位置, S内の終了位置(inclusive), T内の開始位置, T内の終了位置(inclusive)}
+ */
 tuple<int, int, int, int> Longest_Common_Substring(const string &S, const string &T) {
     return Longest_Common_Continuous_Subsequence<char>(vector<char>(S.begin(), S.end()), vector<char>(T.begin(), T.end()));
 }
