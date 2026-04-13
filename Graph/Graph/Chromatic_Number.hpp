1e284d2646f685250a25d4571c27d001e990865f
Kazun1998k <hkazu1998@gmail.com>
2026-04-13 11:39:38 +0900

    高速化

--- Graph/Graph/Chromatic_Number.hpp
+++ Graph/Graph/Chromatic_Number.hpp
@@ -12,38 +12,65 @@ namespace graph {
         using Conv = Bitwise_Or_Convolution<long long>;
         const vector<vector<int>> adj = G.adjacency_matrix();
 
-        // Section I: dp[1][S] を求める. (iff S は独立集合?)
-        Conv indep(vector<long long>(1 << n, 1));
+        // Section I: dp[1][S] を求める. (S が独立集合なら 1, そうでなければ 0)
+        Conv indep(vector<long long>(1 << n, 0));
+        indep[0] = 1;
 
         for (int S = 1; S < (1 << n); ++S) {
             int x = lowest_bit(S); // ビットのインデックスを取得
             int Sx = S ^ (1 << x);
 
-            if (indep[Sx] == 0) {
-                indep[S] = 0;
-                continue;
-            }
+            if (indep[Sx] == 0) continue;
 
+            bool is_indep = true;
             for (int y = 0; y < n; y++) {
                 if (((Sx >> y) & 1) && adj[x][y]) {
-                    indep[S] = 0;
+                    is_indep = false;
                     break;
                 }
             }
+            if (is_indep) indep[S] = 1;
         }
 
-        if (indep[(1 << n) - 1]) return 1;
+        // 空グラフの彩色数は 1
+        if (indep[(1 << n) - 1] == 1) return 1;
 
-        // k 色で彩色可能か包除原理で判定 ( I^k(V) > 0 かを確認 )
-        vector<Conv> dp{Conv(), indep};
-        for (int k = 2; k <= n; ++k) {
-            dp.emplace_back(Conv());
-            dp[k] = dp[1] * dp[k - 1];
+        auto clamp = [&](Conv &c) {
+            for (int S = 0; S < (1 << n); ++S) {
+                c[S] = c[S] > 0 ? 1 : 0;
+            }
+        };
 
-            unless (dp[k][(1 << n) - 1] == 0) return k;
-
-            for (int S = 0; S < (1 << n); ++S) dp[k][S] = dp[k][S] ? 1 : 0;
+        // Section II: k = 2, 4, 8, ... に対して, dp
+        vector<Conv> dp(2 * n, Conv());
+        dp[1] = indep;
+        int k = 1;
+        for (; 2 * k <= n; k *= 2) {
+            dp[2 * k] = dp[k] * dp[k];
+            clamp(dp[2 * k]);
+            if (dp[2 * k][(1 << n) - 1]) break;
         }
-        return n;
+
+        // Section III: 二分探索によって彩色数を求める
+        Conv prev = dp[k];
+        int step = k >> 1;
+
+        for (; step > 0; step >>= 1) {
+            if (k + step > n) continue;
+
+            // prev (dp[k_current]) と dp[step] を畳み込むことで dp[k_current + step] を得る
+            Conv res = prev * dp[step];
+            
+            clamp(res);
+
+            dp[k + step] = res;
+
+            unless(res[(1 << n) - 1]) {
+                k += step;
+                prev = res;
+            }
+        }
+
+        return k + 1;
     }
 }
