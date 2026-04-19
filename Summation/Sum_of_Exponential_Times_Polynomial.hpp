89f1519fc10f5b0a2f6d43322d7355bad43af84c
Kazun1998k <hkazu1998@gmail.com>
2026-04-19 12:32:25 +0900

    Sum_of_Exponential_Times_Polynomial_Limit の移行元を削除

--- Summation/Sum_of_Exponential_Times_Polynomial.hpp
+++ Summation/Sum_of_Exponential_Times_Polynomial.hpp
@@ -65,37 +65,3 @@ template<typename F>
 F Sum_of_Exponential_Times_Polynomial(const F r, const int d, const ll p, const ll q) {
     return Sum_of_Exponential_Times_Polynomial(r, d, q) - Sum_of_Exponential_Times_Polynomial(r, d, p - 1);
 }
-
-template<typename F>
-F Sum_of_Exponential_Times_Polynomial_Limit(const F r, const int d) {
-    assert(r != F(1));
-
-    vector<F> S(d + 2);
-    {
-        vector<F> powers = Enumerate_Powers<F>(d + 2, d);
-        F r_pow = 1;
-        S[0] = powers[0];
-        for (int k = 1; k <= d + 1; ++k) {
-            r_pow *= r;
-            S[k] = S[k - 1] + powers[k] * r_pow;
-        }
-        vector<F>().swap(powers);
-    }
-
-    F P_1 = 0; // 多項式 P(x) の x=1 における値
-    {
-        F comb = 1;
-        F r_pow = 1;
-        for (int k = 0; k <= d + 1; ++k) {
-            F term = comb * r_pow * S[d + 1 - k];
-            is_even(k) ? P_1 += term : P_1 -= term;
-            if (k < d + 1) {
-                comb *= F(d + 1 - k);
-                comb /= F(k + 1);
-                r_pow *= r;
-            }
-        }
-    }
-
-    return P_1 / pow(1 - r, d + 1);
-}
