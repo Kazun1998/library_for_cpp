f345a9f6af2c633495bff657c71e6058ae49cb66
Kazun1998k <hkazu1998@gmail.com>
2026-04-19 12:32:34 +0900

    verify of Sum_of_Exponential_Times_Polynomial_Limit

--- verify/yosupo_library_checker/other/Sum_of_Exponential_Times_Polynomial_Limit.test.cpp
+++ verify/yosupo_library_checker/other/Sum_of_Exponential_Times_Polynomial_Limit.test.cpp
@@ -2,7 +2,7 @@
 
 #include "../../../template/template.hpp"
 #include "../../../Algebra/modint.hpp"
-#include "../../../Summation/Sum_of_Exponential_Times_Polynomial.hpp"
+#include "../../../Summation/Sum_of_Exponential_Times_Polynomial_Limit.hpp"
 
 using mint = modint<998244353>;
 
