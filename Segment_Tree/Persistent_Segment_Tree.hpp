494598b86dd09ef3457c28f8d989a52f055a5cbb
Kazun1998k <hkazu1998@gmail.com>
2026-04-22 23:44:08 +0900

    コンストラクタ実装

--- Segment_Tree/Persistent_Segment_Tree.hpp
+++ Segment_Tree/Persistent_Segment_Tree.hpp
@@ -13,4 +13,31 @@ class Persistent_Segment_Tree {
         Node(M x, int l, int r) : x(x), l(l), r(r), left_child(nullptr), right_child(nullptr) {}
         Node(M x, int l, int r, Node* left, Node* right) : x(x), l(l), r(r), left_child(left), right_child(right) {}
     };
+
+    int n;
+    const function<M(M, M)> op;
+    const M unit;
+    vector<Node*> roots;
+
+    Node* build(const int l, const int r, const vector<M> &data) {
+        // 1 要素区間を表す頂点 → 葉
+        if (r - l == 1) return new Node(data[l], l, r);
+
+        // そうでない場合, 2 要素以上の区間なので, 左と右に分割できる.
+        int m = (l + r) / 2;
+
+        Node* left = build(l, m, data);
+        Node* right = build(m, r, data);
+
+        return new Node(op(left->x, right->x), l, r, left, right);
+    }
+
+    void build_up(const vector<M> &data) {
+        roots.emplace_back(build(0, n, data));
+    }
+
+    public:
+    Persistent_Segment_Tree(const vector<M> &data, const function<M(M, M)> op, const M unit): n(data.size()), op(op), unit(unit) {
+        build_up(data);
+    }
 };
