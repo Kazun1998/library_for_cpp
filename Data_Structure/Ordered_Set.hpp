2ed01a56ec96d0a84d3a485ce62caee28c8c9619
Kazun1998k <hkazu1998@gmail.com>
2026-05-17 14:09:49 +0900

    コンストラクタ実装

--- Data_Structure/Ordered_Set.hpp
+++ Data_Structure/Ordered_Set.hpp
@@ -11,4 +11,7 @@ class Ordered_Set {
     typedef tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update> order_tree;
 
     order_tree t;
+
+    public:
+    Ordered_Set(): t() {};
 };
