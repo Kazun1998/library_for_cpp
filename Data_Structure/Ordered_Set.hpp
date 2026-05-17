2dd3fdbe489ad1512cc652956d627b5e4613d6f8
Kazun1998k <hkazu1998@gmail.com>
2026-05-17 14:09:29 +0900

    インスタンス変数の設定実装

---
+++ Data_Structure/Ordered_Set.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../template/template.hpp"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+template<class T>
+class Ordered_Set {
+    typedef tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update> order_tree;
+
+    order_tree t;
+};
