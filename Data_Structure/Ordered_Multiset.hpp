8951c1cc89422296486734f9fd100c6f3978675b
Kazun1998k <hkazu1998@gmail.com>
2026-05-22 00:24:45 +0900

    add doc

--- Data_Structure/Ordered_Multiset.hpp
+++ Data_Structure/Ordered_Multiset.hpp
@@ -34,11 +34,17 @@ class Ordered_Multiset {
         }
     }
 
+    /// @brief x を挿入する
+    /// @param x 挿入値
+    /// @return 変化があれば true というルールだが, 多重集合としているので必ず true になる.
     bool insert(const T x) {
         t.insert({x, id++});
         return true;
     }
 
+    /// @brief x が存在するならば, x を 1 個消す.
+    /// @param x 削除値
+    /// @return 削除に成功したら `true`.
     bool erase(const T x) {
         auto it = t.lower_bound({x, -1});
         if (it != t.end() && it->first == x) {
@@ -49,26 +55,48 @@ class Ordered_Multiset {
         return false;
     }
 
+    /// @brief x が含まれているか?
+    /// @param x 判定値
+    /// @return x が 1 個以上含まれているならば true.
     bool contains(const T x) const {
         auto it = t.lower_bound({x, -1});
         return it != t.end() && it->first == x;
     }
 
+    /// @brief この多重集合に含まれている元の個数を求める.
+    /// @return 元の個数
     size_t size() const { return t.size(); }
 
+    /// @brief x 未満の元の個数を求める.
+    /// @param x 基準値
+    /// @param equal `true` にすると, 「未満」が「以下」になる.
+    /// @return x 未満の元の数
     int count_less(const T x, bool equal = false) const {
         if (equal) return t.order_of_key({x, id});
         else return t.order_of_key({x, -1});
     }
 
+    /// @brief x より大きい元の個数を求める.
+    /// @param x 基準値
+    /// @param equal `true` にすると, 「より大きい」が「以上」になる.
+    /// @return x より大きい元の個数
     int count_more(const T x, bool equal = false) const {
         return size() - count_less(x, !equal);
     }
 
+    /// @brief x の個数 (重複度) を求める
+    /// @param x 判定値
+    /// @return x の個数 (重複度) を求める.
     int count(const T x) const {
         return count_less(x, true) - count_less(x, false);
     }
 
+    /// @brief l 以上 r 未満の元の数を求める.
+    /// @param l 下限
+    /// @param r 上限
+    /// @param l_equal 下限の条件に等号を認めるか?
+    /// @param r_equal 上限の条件に等号を認めるか?
+    /// @return l 以上 r 未満の元の数
     int count_range(const T l, const T r, bool l_equal = true, bool r_equal = false) const {
         if (l > r || (l == r && (!l_equal || !r_equal))) return 0;
 
@@ -77,6 +105,9 @@ class Ordered_Multiset {
         return std::max(0, upper - lower);
     }
 
+    /// @brief x を消せるだけ消す.
+    /// @param x 削除値
+    /// @return 削除した x の個数.
     int discard(const T x) {
         if (!contains(x)) return 0;
 
@@ -94,11 +125,17 @@ class Ordered_Multiset {
         return tail.size();;
     }
 
+    /// @brief k (0-indexed) 番目の値を求める.
+    /// @param k 番号
+    /// @return k 番目の値
     T kth(int k) const {
         if (k < 0) k += size();
         return t.find_by_order(k)->first;
     }
 
+    /// @brief k (0-indexed) 番目の値を求める. 範囲外の時は nullopt.
+    /// @param k 番号
+    /// @return k 番目の値
     optional<T> safe_kth(int k) const {
         if (k < 0) k += size();
 
@@ -114,17 +151,31 @@ class Ordered_Multiset {
     T kth_min(int k) const { return kth(k); }
     T kth_max(int k) const { return kth(-(k + 1)); }
 
+    /// @return 最大値
     T min() const { return kth(0); }
+
+    /// @return 最小値
     T max() const { return kth(-1); }
 
+    /// @brief x より大きい最小の値を求める.
+    /// @param x 基準値
+    /// @param equal `true` にすると, 「より大きい」が「以上」になる.
+    /// @return x より大きい最小の値
     std::optional<T> next(const T x, bool equal = false) const {
         return safe_kth(count_less(x, !equal));
     }
 
+    /// @brief x 未満の最大の値を求める.
+    /// @param x 基準値
+    /// @param equal `true` にすると, 「未満が「以下」になる.
+    /// @return x 以下の最大の値
     std::optional<T> previous(const T x, bool equal = false) const {
         return safe_kth(count_less(x, equal) - 1);
     }
 
+    /// @return 空集合?
     bool empty() const { return t.empty(); }
+
+    /// @brief 初期化
     void clear() { t.clear(); id = 0; }
 };
