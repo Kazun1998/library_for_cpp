5194312d06a70cf1f7b8968068afc1c12a4c547d
Kazun1998k <hkazu1998@gmail.com>
2026-05-22 00:31:37 +0900

    gemini

--- Data_Structure/Ordered_Multiset.hpp
+++ Data_Structure/Ordered_Multiset.hpp
@@ -6,13 +6,13 @@
 
 using namespace __gnu_pbds;
 
-template<class T>
+template<class T, class I = int>
 class Ordered_Multiset {
-    using Key = pair<T, int>;
-    typedef tree<Key, null_type, less<Key>, rb_tree_tag, tree_order_statistics_node_update> order_tree;
+    using Key = pair<T, I>;
+    using order_tree = tree<Key, null_type, less<Key>, rb_tree_tag, tree_order_statistics_node_update>;
 
     order_tree t;
-    int id = 0;
+    I id = 0;
 
     public:
     // コンストラクタ
@@ -41,7 +41,7 @@ class Ordered_Multiset {
     /// @brief x を挿入する
     /// @param x 挿入値
     /// @return 変化があれば true というルールだが, 多重集合としているので必ず true になる.
-    bool insert(const T x) {
+    bool insert(const T& x) {
         t.insert({x, id++});
         return true;
     }
@@ -51,7 +51,7 @@ class Ordered_Multiset {
     /// @brief x が存在するならば, x を 1 個消す.
     /// @param x 削除値
     /// @return 削除に成功したら `true`.
-    bool erase(const T x) {
+    bool erase(const T& x) {
         auto it = t.lower_bound({x, -1});
         if (it != t.end() && it->first == x) {
             t.erase(it);
@@ -64,7 +64,7 @@ class Ordered_Multiset {
     /// @brief x を消せるだけ消す.
     /// @param x 削除値
     /// @return 削除した x の個数.
-    int discard(const T x) {
+    int discard(const T& x) {
         if (!contains(x)) return 0;
 
         order_tree tail, more;
@@ -84,7 +84,7 @@ class Ordered_Multiset {
     /// @brief x が含まれているか?
     /// @param x 判定値
     /// @return x が 1 個以上含まれているならば true.
-    bool contains(const T x) const {
+    bool contains(const T& x) const {
         auto it = t.lower_bound({x, -1});
         return it != t.end() && it->first == x;
     }
@@ -99,8 +99,8 @@ class Ordered_Multiset {
     /// @param x 基準値
     /// @param equal `true` にすると, 「未満」が「以下」になる.
     /// @return x 未満の元の数
-    int count_less(const T x, bool equal = false) const {
-        if (equal) return t.order_of_key({x, id});
+    int count_less(const T& x, bool equal = false) const { // x 未満の元の個数を求める.
+        if (equal) return t.order_of_key({x, std::numeric_limits<I>::max()}); // x 以下の個数を求めるために、十分大きな ID を指定
         else return t.order_of_key({x, -1});
     }
 
@@ -108,14 +108,14 @@ class Ordered_Multiset {
     /// @param x 基準値
     /// @param equal `true` にすると, 「より大きい」が「以上」になる.
     /// @return x より大きい元の個数
-    int count_more(const T x, bool equal = false) const {
+    int count_more(const T& x, bool equal = false) const {
         return size() - count_less(x, !equal);
     }
 
     /// @brief x の個数 (重複度) を求める
     /// @param x 判定値
     /// @return x の個数 (重複度) を求める.
-    int count(const T x) const {
+    int count(const T& x) const {
         return count_less(x, true) - count_less(x, false);
     }
 
@@ -125,7 +125,7 @@ class Ordered_Multiset {
     /// @param l_equal 下限の条件に等号を認めるか?
     /// @param r_equal 上限の条件に等号を認めるか?
     /// @return l 以上 r 未満の元の数
-    int count_range(const T l, const T r, bool l_equal = true, bool r_equal = false) const {
+    int count_range(const T& l, const T& r, bool l_equal = true, bool r_equal = false) const {
         if (l > r || (l == r && (!l_equal || !r_equal))) return 0;
 
         int upper = count_less(r, r_equal);
@@ -161,10 +161,10 @@ class Ordered_Multiset {
     T kth_min(int k) const { return kth(k); }
     T kth_max(int k) const { return kth(-(k + 1)); }
 
-    /// @return 最大値
+    /// @return 最小値
     T min() const { return kth(0); }
 
-    /// @return 最小値
+    /// @return 最大値
     T max() const { return kth(-1); }
 
     // 隣接要素系メソッド
@@ -173,15 +173,15 @@ class Ordered_Multiset {
     /// @param x 基準値
     /// @param equal `true` にすると, 「より大きい」が「以上」になる.
     /// @return x より大きい最小の値
-    std::optional<T> next(const T x, bool equal = false) const {
+    std::optional<T> next(const T& x, bool equal = false) const {
         return safe_kth(count_less(x, !equal));
     }
 
     /// @brief x 未満の最大の値を求める.
     /// @param x 基準値
-    /// @param equal `true` にすると, 「未満が「以下」になる.
+    /// @param equal `true` にすると, 「未満」が「以下」になる.
     /// @return x 以下の最大の値
-    std::optional<T> previous(const T x, bool equal = false) const {
+    std::optional<T> previous(const T& x, bool equal = false) const {
         return safe_kth(count_less(x, equal) - 1);
     }
 
