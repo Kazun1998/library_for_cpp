d074729ef40e10b18d027935e0509965d0d597dd
Kazun1998k <hkazu1998@gmail.com>
2026-05-30 18:13:59 +0900

    基本的なメソッドを実装

---
+++ Imos/Linear_Imos_1.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+template <typename T>
+class Linear_Imos_1 {
+    private:
+    std::vector<T> slope;
+    std::vector<T> intercept;
+    int n;
+
+    void push(int t, T a, T b) {
+        if (!(0 <= t && t <= n)) return;
+        intercept[t] += a;
+        slope[t] += b;
+    }
+
+    public:
+    Linear_Imos_1(const int n) : n(n), slope(n + 1, 0), intercept(n + 1, 0) {}
+
+    /// @brief 閉区間 [l, r] の各要素 i に対し、a * (i - l) + b を加算
+    /// @param l 左端
+    /// @param r 右端
+    /// @param a 傾き (公差)
+    /// @param b 定数項 (初項)
+    void add(int l, int r, const T a, const T b) {
+        if (l > r) return;
+
+        // 範囲の丸め込み
+        if (l < 0) l = 0;
+        if (r >= n) r = n - 1;
+        if (l > r || l >= n || r < 0) return;
+
+        // a(i - l) + b = a*i + (b - a*l)
+        push(l, b - a * l, a);
+        push(r + 1, -(b - a * l), -a);
+    }
+
+    /// @brief 閉区間 [l, r] に定数 x を加算
+    void add(int l, int r, const T x) { add(l, r, 0, x); }
+
+    /// @brief 閉区間 [l, r] に 1, 2, 3, ... (公差1, 初項1) を加算
+    void add(int l, int r) { add(l, r, T(1), T(1)); }
+
+    inline int size() const { return n; }
+
+    /// @brief 累積和を計算する
+    std::vector<T> cumulate() {
+        if (n == 0) return std::vector<T>();
+
+        std::vector<T> res(n);
+        T s = 0, ic = 0;
+        for (int i = 0; i < n; ++i) {
+            s += slope[i];
+            ic += intercept[i];
+            res[i] = s * i + ic;
+        }
+
+        return res;
+    }
+
+    /// @brief 内部状態をクリアして、再度 add できる状態にする
+    void clear() {
+        std::fill(slope.begin(), slope.end(), T(0));
+        std::fill(intercept.begin(), intercept.end(), T(0));
+    }
+};
