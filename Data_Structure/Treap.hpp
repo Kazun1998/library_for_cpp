32ab8193a83602fe899f43da4e320d5c3d5ed5dd
Kazun1998k <hkazu1998@gmail.com>
2026-06-14 21:01:45 +0900

    size 関連の機能を実装

--- Data_Structure/Treap.hpp
+++ Data_Structure/Treap.hpp
@@ -7,25 +7,35 @@ class Treap {
     struct Node {
         T key;
         int priority;
+        int size;
         Node *left = nullptr, *right = nullptr;
-        Node(T key, int priority) : key(key), priority(priority) {}
+        Node(T key, int priority) : key(key), priority(priority), size(1) {}
     };
 
     Node *root = nullptr;
     std::mt19937 mt;
 
+    static int get_size(Node* node) { return node ? node->size : 0; }
+
+    static Node* update(Node* node) {
+        if (node) node->size = 1 + get_size(node->left) + get_size(node->right);
+        return node;
+    }
+
     public:
     Treap(): mt(std::random_device{}()) {}
+
+    int size() const { return get_size(root); }
 
     static Node* merge(Node* first, Node* second) {
         if (!first || !second) return first ? first : second;
 
         if (first->priority > second->priority) {
             first->right = merge(first->right, second);
-            return first;
+            return update(first);
         } else {
             second->left = merge(first, second->left);
-            return second;
+            return update(second);
         }
     }
 
@@ -34,10 +44,10 @@ class Treap {
 
         if (tree->key < key) {
             split(tree->right, key, tree->right, more);
-            less = tree;
+            less = update(tree);
         } else {
             split(tree->left, key, less, tree->left);
-            more = tree;
+            more = update(tree);
         }
     }
 
