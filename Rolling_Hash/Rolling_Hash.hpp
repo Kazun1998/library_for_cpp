2574e2d2201c2b4feb7fb2b7dea7eec74451646a
Kazun1998k <hkazu1998@gmail.com>
2026-08-09 21:17:44 +0900

    Rolling_Hash_Base を作成

--- Rolling_Hash/Rolling_Hash.hpp
+++ Rolling_Hash/Rolling_Hash.hpp
@@ -1,23 +1,19 @@
 #pragma once
 
 #include "../template/template.hpp"
+#include "Rolling_Hash_Base.hpp"
 
 namespace rolling_hash {
     template<typename F>
-    class Rolling_Hash {
-        const F base;
-        int _size;
-        vector<F> power;
-        vector<F> hash;
+    class Rolling_Hash : public Rolling_Hash_Base<F, Rolling_Hash<F>> {
+        using Base = Rolling_Hash_Base<F, Rolling_Hash<F>>;
+        friend Base;
 
-        F hashing(const vector<F> &X) const {
-            F res(0);
-            for (F a: X) {
-                res = base * res + a;
-            }
-
-            return res;
-        }
+        F one() const { return F(1); }
+        F zero() const { return F(0); }
+        F mul(const F &a, const F &b) const { return a * b; }
+        F add(const F &a, const F &b) const { return a + b; }
+        F sub(const F &a, const F &b) const { return a - b; }
 
         static vector<F> to_vector(const string &S) {
             vector<F> data(S.size());
@@ -26,62 +22,11 @@ namespace rolling_hash {
         }
 
         public:
-        Rolling_Hash(const vector<F> &data, const F base): base(base), _size(data.size()) {
-            power.resize(size() + 1);
-            hash.resize(size() + 1);
+        using Base::count;
+        using Base::find;
+        using Base::rfind;
 
-            power[0] = F(1);
-            hash[0] = F(0);
-            for (int i = 0; i < size(); ++i) {
-                power[i + 1] = base * power[i];
-                hash[i + 1] = base * hash[i] + data[i];
-            }
-        }
-
-        int size() const { return _size; }
-
-        F get_base() const { return base; }
-
-        /// @brief [0, p) におけるハッシュ値を求める
-        /// @param p 右端 (p は含まれない)
-        F get(const int p) const {
-            return hash[p];
-        }
-
-        /// @brief [l, r) におけるハッシュ値を求める
-        /// @param l 左端 (l は含まれる)
-        /// @param r 右端 (r は含まれない)
-        F get(const int l, const int r) const {
-            return get(r) - power[r - l] * get(l);
-        }
-
-        int count(const vector<F> &X, int start) const {
-            F alpha = hashing(X);
-            int res = 0;
-            for (int i = start; i <= size() - (int)X.size(); ++i) {
-                if (alpha == get(i, i + X.size())) res++;
-            }
-
-            return res;
-        }
-
-        int count(const vector<F> &X) const { return count(X, 0); }
-
-        int find(const vector<F> &X, int start) const {
-            F alpha = hashing(X);
-            for (int i = start; i <= size() - (int)X.size(); ++i) {
-                if (alpha == get(i, i + X.size())) return i;
-            }
-
-            return -1;
-        }
-
-        int find(const vector<F> &X) const { return find(X, 0); }
-
-        /// @brief 区間 [l0, r0) と [l1, r1) をこの順に連結したときのハッシュ値を求める
-        F docking(const int l0, const int r0, const int l1, const int r1) const {
-            return get(l0, r0) * power[r1 - l1] + get(l1, r1);
-        }
+        Rolling_Hash(const vector<F> &data, const F base): Base(base) { this->build(data); }
 
         // 文字列用オーバーロード
         Rolling_Hash(const string &S, const F base): Rolling_Hash(to_vector(S), base) {}
@@ -91,6 +36,9 @@ namespace rolling_hash {
 
         int find(const string &X, int start) const { return find(to_vector(X), start); }
         int find(const string &X) const { return find(to_vector(X), 0); }
+
+        int rfind(const string &X, int start) const { return rfind(to_vector(X), start); }
+        int rfind(const string &X) const { return rfind(to_vector(X), 0); }
     };
 
     // static な mod() と, 剰余を表すメンバ x を持つ型 (modint 想定)
@@ -178,45 +126,54 @@ namespace rolling_hash {
             return encode(rh0.docking(l0, r0, l1, r1), rh1.docking(l0, r0, l1, r1));
         }
 
-        template<typename T>
-        int count(const vector<T> &X, int start) const {
-            P alpha = hashing(X);
+        private:
+        // X は vector<T> または string を想定 (hashing のオーバーロードが解決できること)
+        template<typename X>
+        int count_impl(const X &pattern, int start) const {
+            P alpha = hashing(pattern);
             int res = 0;
-            for (int i = start; i <= size() - (int)X.size(); ++i) {
-                if (alpha == get(i, i + X.size())) res++;
+            for (int i = start; i <= size() - (int)pattern.size(); ++i) {
+                if (alpha == get(i, i + pattern.size())) res++;
             }
 
             return res;
         }
 
-        template<typename T>
-        int count(const vector<T> &X) const { return count(X, 0); }
-
-        template<typename T>
-        int find(const vector<T> &X, int start) const {
-            P alpha = hashing(X);
-            for (int i = start; i <= size() - (int)X.size(); ++i) {
-                if (alpha == get(i, i + X.size())) return i;
+        template<typename X>
+        int find_impl(const X &pattern, int start) const {
+            P alpha = hashing(pattern);
+            for (int i = start; i <= size() - (int)pattern.size(); ++i) {
+                if (alpha == get(i, i + pattern.size())) return i;
             }
 
             return -1;
         }
 
-        template<typename T>
-        int find(const vector<T> &X) const { return find(X, 0); }
-
-        template<typename T>
-        int rfind(const vector<T> &X, int start) const {
-            P alpha = hashing(X);
-            for (int i = size() - (int)X.size(); i >= start; --i) {
-                if (alpha == get(i, i + X.size())) return i;
+        template<typename X>
+        int rfind_impl(const X &pattern, int start) const {
+            P alpha = hashing(pattern);
+            for (int i = size() - (int)pattern.size(); i >= start; --i) {
+                if (alpha == get(i, i + pattern.size())) return i;
             }
 
             return -1;
         }
 
+        public:
         template<typename T>
-        int rfind(const vector<T> &X) const { return rfind(X, 0); }
+        int count(const vector<T> &X, int start) const { return count_impl(X, start); }
+        template<typename T>
+        int count(const vector<T> &X) const { return count_impl(X, 0); }
+
+        template<typename T>
+        int find(const vector<T> &X, int start) const { return find_impl(X, start); }
+        template<typename T>
+        int find(const vector<T> &X) const { return find_impl(X, 0); }
+
+        template<typename T>
+        int rfind(const vector<T> &X, int start) const { return rfind_impl(X, start); }
+        template<typename T>
+        int rfind(const vector<T> &X) const { return rfind_impl(X, 0); }
 
         template<typename T>
         int index(const vector<T> &X, int start) const {
@@ -224,7 +181,6 @@ namespace rolling_hash {
             assert(i != -1);
             return i;
         }
-
         template<typename T>
         int index(const vector<T> &X) const { return index(X, 0); }
 
@@ -232,46 +188,20 @@ namespace rolling_hash {
         Double_Rolling_Hash(const string &S, const long long base):
             _size(S.size()), rh0(S, F0(base)), rh1(S, F1(base)) {}
 
-        int count(const string &X, int start) const {
-            P alpha = hashing(X);
-            int res = 0;
-            for (int i = start; i <= size() - (int)X.size(); ++i) {
-                if (alpha == get(i, i + X.size())) res++;
-            }
+        int count(const string &X, int start) const { return count_impl(X, start); }
+        int count(const string &X) const { return count_impl(X, 0); }
 
-            return res;
-        }
+        int find(const string &X, int start) const { return find_impl(X, start); }
+        int find(const string &X) const { return find_impl(X, 0); }
 
-        int count(const string &X) const { return count(X, 0); }
-
-        int find(const string &X, int start) const {
-            P alpha = hashing(X);
-            for (int i = start; i <= size() - (int)X.size(); ++i) {
-                if (alpha == get(i, i + X.size())) return i;
-            }
-
-            return -1;
-        }
-
-        int find(const string &X) const { return find(X, 0); }
-
-        int rfind(const string &X, int start) const {
-            P alpha = hashing(X);
-            for (int i = size() - (int)X.size(); i >= start; --i) {
-                if (alpha == get(i, i + X.size())) return i;
-            }
-
-            return -1;
-        }
-
-        int rfind(const string &X) const { return rfind(X, 0); }
+        int rfind(const string &X, int start) const { return rfind_impl(X, start); }
+        int rfind(const string &X) const { return rfind_impl(X, 0); }
 
         int index(const string &X, int start) const {
             int i = find(X, start);
             assert(i != -1);
             return i;
         }
-
         int index(const string &X) const { return index(X, 0); }
     };
 } // namespace rolling_hash
---
+++ Rolling_Hash/Rolling_Hash_Base.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+namespace rolling_hash {
+    /// @brief Rolling_Hash 系のクラスに共通する処理をまとめた基底クラス (CRTP)
+    /// @tparam F ハッシュ値の型
+    /// @tparam Derived 実際の演算 (one, zero, mul, add, sub) を提供する派生クラス
+    template<typename F, typename Derived>
+    class Rolling_Hash_Base {
+        protected:
+        F base;
+        int _size;
+        vector<F> power;
+        vector<F> hash;
+
+        const Derived& self() const { return static_cast<const Derived&>(*this); }
+
+        /// @brief data から power, hash テーブルを構築する
+        void build(const vector<F> &data) {
+            _size = (int)data.size();
+
+            power.assign(size() + 1, self().one());
+            hash.assign(size() + 1, self().zero());
+
+            for (int i = 0; i < size(); ++i) {
+                power[i + 1] = self().mul(base, power[i]);
+                hash[i + 1] = self().add(self().mul(base, hash[i]), data[i]);
+            }
+        }
+
+        Rolling_Hash_Base(const F base): base(base) {}
+
+        public:
+        int size() const { return _size; }
+
+        F get_base() const { return base; }
+
+        /// @brief [0, p) におけるハッシュ値を求める
+        /// @param p 右端 (p は含まれない)
+        F get(const int p) const { return hash[p]; }
+
+        /// @brief [l, r) におけるハッシュ値を求める
+        /// @param l 左端 (l は含まれる)
+        /// @param r 右端 (r は含まれない)
+        F get(const int l, const int r) const {
+            return self().sub(get(r), self().mul(power[r - l], get(l)));
+        }
+
+        /// @brief 区間 [l0, r0) と [l1, r1) をこの順に連結したときのハッシュ値を求める
+        F docking(const int l0, const int r0, const int l1, const int r1) const {
+            return self().add(self().mul(get(l0, r0), power[r1 - l1]), get(l1, r1));
+        }
+
+        /// @brief X 全体のハッシュ値を求める
+        F hashing(const vector<F> &X) const {
+            F res = self().zero();
+            for (const F &a: X) {
+                res = self().add(self().mul(base, res), a);
+            }
+
+            return res;
+        }
+
+        int count(const vector<F> &X, int start) const {
+            F alpha = hashing(X);
+            int res = 0;
+            for (int i = start; i <= size() - (int)X.size(); ++i) {
+                if (alpha == get(i, i + X.size())) res++;
+            }
+
+            return res;
+        }
+
+        int count(const vector<F> &X) const { return count(X, 0); }
+
+        int find(const vector<F> &X, int start) const {
+            F alpha = hashing(X);
+            for (int i = start; i <= size() - (int)X.size(); ++i) {
+                if (alpha == get(i, i + X.size())) return i;
+            }
+
+            return -1;
+        }
+
+        int find(const vector<F> &X) const { return find(X, 0); }
+
+        int rfind(const vector<F> &X, int start) const {
+            F alpha = hashing(X);
+            for (int i = size() - (int)X.size(); i >= start; --i) {
+                if (alpha == get(i, i + X.size())) return i;
+            }
+
+            return -1;
+        }
+
+        int rfind(const vector<F> &X) const { return rfind(X, 0); }
+    };
+} // namespace rolling_hash
