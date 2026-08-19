455b7f3cc97fa241e2e8decaa134f86de60c77cd
Kazun1998k <hkazu1998@gmail.com>
2026-08-20 00:51:27 +0900

    cond の型を auto に拡大

--- Binary_Search/General_Integer.hpp
+++ Binary_Search/General_Integer.hpp
@@ -5,11 +5,11 @@
 /// @tparam T 整数型
 /// @param L: 下端
 /// @param R: 上端
-/// @param cond: [L, R] 上広義単調増加な条件
+/// @param cond: [L, R] 上広義単調増加な条件. T を受け取り bool を返す呼び出し可能な値.
 /// @param default_value: cond(R) が false の時の返り値
 /// @return cond(x) が true になる最小の整数 x. ただし, cond(R) が false の場合は default_value を返す.
 template<typename T>
-T General_Binary_Increase_Search_Integer(T L, T R, const function<bool(T)> cond, T default_value) {
+T General_Binary_Increase_Search_Integer(T L, T R, const auto& cond, T default_value) {
     // 例外ケースの処理
     // R でも false → 異常値
     unless(cond(R)) { return default_value; }
@@ -29,11 +29,11 @@ T General_Binary_Increase_Search_Integer(T L, T R, const function<bool(T)> cond,
 /// @tparam T 整数型
 /// @param L: 下端
 /// @param R: 上端
-/// @param cond: [L, R] 上広義単調減少な条件
+/// @param cond: [L, R] 上広義単調減少な条件. T を受け取り bool を返す呼び出し可能な値.
 /// @param default_value: cond(L) が false の時の返り値
 /// @return cond(x) が true になる最大の整数 x. ただし, cond(L) が false の場合は default_value を返す.
 template<typename T>
-T General_Binary_Decrease_Search_Integer(T L, T R, const function<bool(T)> cond, T default_value) {
+T General_Binary_Decrease_Search_Integer(T L, T R, const auto& cond, T default_value) {
     // 例外ケースの処理
     // L でも false → 異常値
     unless(cond(L)) { return default_value; }
