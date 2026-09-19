48b00c613999e2a4b6bd7ea3a2b785f21674a19b
Kazun1998k <hkazu1998@gmail.com>
2026-09-19 23:27:31 +0900

    Range_Min_Segment_Tree, Range_Max_Segment_Tree 実装

---
+++ Segment_Tree/preset/Range_Max.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../Segment_Tree.hpp"
+
+template<typename T>
+class Range_Max_Segment_Tree : public Segment_Tree<T> {
+    static T op(T x, T y) { return x < y ? y : x; }
+
+    public:
+    Range_Max_Segment_Tree(int n, T unit) : Segment_Tree<T>(n, op, unit) {}
+
+    Range_Max_Segment_Tree(const vector<T> &vec, T unit) : Segment_Tree<T>(vec, op, unit) {}
+
+    void update(int k, T x) { Segment_Tree<T>::update(k, x); }
+
+    // 区間 [l, r] の最大値を求める
+    T max(int l, int r) { return this->product(l, r); }
+};
---
+++ Segment_Tree/preset/Range_Min.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../Segment_Tree.hpp"
+
+template<typename T>
+class Range_Min_Segment_Tree : public Segment_Tree<T> {
+    static T op(T x, T y) { return x < y ? x : y; }
+
+    public:
+    Range_Min_Segment_Tree(int n, T unit) : Segment_Tree<T>(n, op, unit) {}
+
+    Range_Min_Segment_Tree(const vector<T> &vec, T unit) : Segment_Tree<T>(vec, op, unit) {}
+
+    void update(int k, T x) { Segment_Tree<T>::update(k, x); }
+
+    // 区間 [l, r] の最小値を求める
+    T min(int l, int r) { return this->product(l, r); }
+};
